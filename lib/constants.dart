76db9cbb22308a7b44ac3eaa44dcbe0267a819fe
Adeife Taiwo <adeifetaiwo50@gmail.com>
2023-09-13 08:02:34 +0100

    flutter ui challenge

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,6 +2,6 @@ import 'dart:ui';
 
 const kTextColor = Color(0xFF535353);
 const kTextLightColor = Color(0xFFACACAC);
-const kTextPadding = 20.0;
+const kTextPadding = 20.1;
 const kDefaultPadding = 20.0;
 const kBgColor = Color(0xFF0F282C);
